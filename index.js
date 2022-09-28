a17f13689ade7269d9ef0fe16085511414c1e5cc
Sejin <kisejin2015@gmail.com>
2022-09-28 09:18:17 -0600

    Dispay empl and dept working

--- index.js
+++ index.js
@@ -1,21 +1,86 @@
 const inquirer = require('inquirer');
 const mysql = require("mysql2");
-const table = require("console.table");
+require("console.table");
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: '',
-      database: 'classlist_db'
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'EMPLOYEE_TRACKER'
     })
 
-    db.connect(function(){
-    console.log(`Connected to the classlist_db database.`)
+db.connect(function () {
+    console.log(`Connected to the database.`)
     employeeTracker()
-    });
-  function employeeTracker(){
+});
+
+function employeeTracker() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Employee Tracker Application",
+            name: "options",
+            choices: ["View Department",
+            "View Roles",
+            "View Employees",
+            "Add Departments",
+            "Add Roles",
+            "Add Employees",
+            "Update Employee Role", 
+            "Exit Application"]
+        }
+    ]).then(response => {
+        switch (response.options) {
+            case "View Department":
+                displayDepartment();
+                break;
+            case "View Roles":
+                displayRoles();
+                break;
+            case "View Employees":
+                displayEmployees();
+                break;
+            case "Add Departments":
+                displayAddDepartments();
+                break;
+            case "Add Roles":
+                displayAddRoles();
+                break;
+            case "Add Employees":
+                displayAddEmployees();
+                break;
+            case "Update Employee Role":
+                displayUpdateEmployeeRole();
+                break;
+            case "Exit Application":
+                db.end()
+                process.exit(0)
+
+        }
+    })
     
+}
+
+
+function displayDepartment() {
+    console.log("Display Department")
+    db.query("select * from department;", function (err, data) {
+        if (err) throw err;
+        console.log(err,data)
+        console.table(data);
+        employeeTracker()
+    })
+}
+
+
+  function displayEmployees(){
+    console.log("Employee Details")
+    db.query("select * from employee",function(err,data){
+        if(err) throw err;
+        console.table(data);
+        employeeTracker()
+    })
   }
